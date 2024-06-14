bcce765afe5f5d3a5a0341274a49a77c6a928b56
Jay Jariwala <jay@Jays-MacBook-Pro.local>
2024-06-14 16:03:55 +0530

    address env error fix

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 
 const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface),CONTRACT_ADDRESS);
 
